708ef5e49dc97f4597aef1a8945bf5bf468bb11d
Scarlet1107 <shougo.ushiro@gmail.com>
2024-03-06 21:30:15 +0900

    アイコンアップロードを半分実装

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Compressor from "compressorjs";
 import React, { useState } from "react";
 import { useCookies } from "react-cookie";
 import { useDispatch, useSelector } from "react-redux";
@@ -17,6 +18,7 @@ export const SignUp = () => {
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessge] = useState();
   const [cookies, setCookie, removeCookie] = useCookies();
+  const [icon, setIcon] = useState();
   const handleEmailChange = (e) => setEmail(e.target.value);
   const handleNameChange = (e) => setName(e.target.value);
   const handlePasswordChange = (e) => setPassword(e.target.value);
@@ -25,6 +27,12 @@ export const SignUp = () => {
       email: email,
       name: name,
       password: password
+    };
+
+    //2024/3/6編集中
+    const iconData = {
+      icon: icon
+
     };
 
     axios.post(`${url}/users`, data)
@@ -38,8 +46,33 @@ export const SignUp = () => {
         setErrorMessge(`サインアップに失敗しました。 ${err}`);
       })
 
-      if(auth) return <Navigate to="/" />
+    if (auth) return <Navigate to="/" />
   }
+
+  const handleIconChange = (e) => {
+    const file = e.target.files[0];
+
+    new Compressor(file, {
+      quality: 0.6, //圧縮品質を0.6に設定
+      success(result) {
+        setIcon(result);　//圧縮後の状態を保存
+      },
+      error(err) {
+        console.log(err);
+      },
+    });
+
+    axios.post(`${url}/uploads`, icon,)
+      .then((res) => {
+        const iconUrl = res.data.url;
+        setIcon(iconUrl);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+
+  };
+
   return (
     <div>
       <Header />
@@ -53,7 +86,9 @@ export const SignUp = () => {
           <input type="text" onChange={handleNameChange} className="name-input" /><br />
           <label>パスワード</label><br />
           <input type="password" onChange={handlePasswordChange} className="password-input" /><br />
-          
+
+          <p>アイコンをアップロード</p>
+          <input type="file" onChange={handleIconChange} className="file-input" />
 
           <button type="button" onClick={onSignUp} className="signup-button">作成</button>
         </form>
