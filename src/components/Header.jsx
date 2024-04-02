8d7e635e29ea49c59e467445d040808c7f503e9c
scarlet7 <shougo.ushiro@gmail.com>
2024-04-03 01:25:55 +0900

    ヘッダー部分を編集中。配置をうまくしたい

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,17 +3,33 @@ import { useCookies } from "react-cookie";
 import { useDispatch, useSelector } from "react-redux";
 import { useLocation, useNavigate } from "react-router-dom";
 import { signOut } from "../authSlice";
+import SWR from "swr";
 import "../index.css";
 
 export const Header = () => {
-
   const location = useLocation();
   const auth = useSelector((state) => state.auth.isSignIn);
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const [cookies, setCookie, removeCookie] = useCookies();
+  const [cookies, setCookie, removeCookie] = useCookies(["token"]);
 
+  const apiUrl = process.env.REACT_APP_API_URL;
 
+  async function fetcher(key) {
+    const res = await fetch(key, {
+      headers: {
+        Authorization: `Bearer ${cookies.token}`,
+      },
+    });
+    return res.json();
+  }
+
+  const { data, error } = SWR(`${apiUrl}/users`, fetcher);
+  if (error) {
+    return <div className="text-red-500">Error : ユーザー情報の取得に失敗しました</div>;
+  } else if (!data) { 
+    return <div className="text-4xl p-8 bg-blue-400">Now loading...</div>;
+  }
 
   const handleSignOut = () => {
     dispatch(signOut());
@@ -21,21 +37,34 @@ export const Header = () => {
     navigate("/signin");
   };
 
-
   return (
-    <header className="flex justify-around bg-blue-400">
-      <h1 className="font-bold text-4xl p-8 mr-32">書籍レビューアプリ</h1>
+    <header className="flex bg-blue-400">
+      <h1 className="font-bold text-4xl p-8">書籍レビューアプリ</h1>
+      <div className="flex justify-end py-2 px-4 m-8 text-xl">おかえりなさい！{data && data.name}さん</div>
       {/*もしURLがsigninまたはsignupなら下のsign-out-buttonを表示しない*/}
       {auth ? (
-        location.pathname === "/signin" ? null :
-        <button className="button ml-32 text-xl" onClick={handleSignOut}>
-          サインアウト
+        location.pathname === "/signin" ? null : (
+          <button className="button text-xl flex justify-end" onClick={handleSignOut}>
+            サインアウト
+          </button>
+        )
+      ) : location.pathname === "/signin" ? (
+        <button
+          type="button"
+          onClick={() => navigate("/signup")}
+          className="button text-xl"
+        >
+          アカウントを新規作成
         </button>
       ) : (
-        location.pathname === "/signin" ? 
-        <button type="button" onClick={() => navigate("/signup")} className="button ml-32 text-xl">アカウントを新規作成</button> : 
-          <button type="button" onClick={() => navigate("/signin")} className="button ml-32 text-xl">アカウントをお持ちの方</button>
+        <button
+          type="button"
+          onClick={() => navigate("/signin")}
+          className="button text-xl"
+        >
+          アカウントをお持ちの方
+        </button>
       )}
-    </header>  
+    </header>
   );
 };
