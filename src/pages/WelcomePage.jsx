55119439f1b294e3fa0c0eef10c86380fcbad4db
scarlet7 <shougo.ushiro@gmail.com>
2024-04-04 02:32:48 +0900

    WelcomePageを作成中。３０％完成

---
+++ src/pages/WelcomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../index.css";
+import { Header } from "../components/Header";
+
+export const WelcomePage = () => {
+  return (
+    <div>
+      <Header />
+      {/* コンポーネントの内容をここに書く */}
+    </div>
+  );
+};
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -4,9 +4,9 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { Home } from "../pages/Home";
 import { SignIn } from "../pages/SignIn";
 import { SignUp } from "../pages/SignUp";
+import { WelcomePage } from "../pages/WelcomePage";
 
 export const Router = () => {
-
   const auth = useSelector((state) => state.auth.isSignIn);
 
   return (
@@ -14,13 +14,14 @@ export const Router = () => {
       <Routes>
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/" element={<WelcomePage />} />
         {auth ? (
           <>
             <Route path="/home" element={<Home />} />
             <Route path="*" element={<Navigate to="/home" />} />
           </>
         ) : (
-          <Route path="*" element={<Navigate to="/signin" />} />
+          <Route path="*" element={<Navigate to="/" />} />
         )}
       </Routes>
     </BrowserRouter>
